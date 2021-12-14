9102fa8aa0b8dab5ec4a72bd7647a4bcb7e62418
Shailesh Pranav Rajendran <shaileshpranav.r@gmail.com>
2021-12-14 05:21:06 -0500

    Explorer almost done

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@
 #include <xmlrpcpp/XmlRpcValue.h>
 #include <std_msgs/String.h>
 #include <iostream>
+#include <array>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int32_t fid_id = 10;
+
+
+int32_t fid_id;
 
 void broadcast() {
   //for broadcaster
@@ -34,7 +37,32 @@ void broadcast() {
   ROS_INFO("Broadcasting");
   br.sendTransform(transformStamped);
 }
+std::array<std::array<int,2>,4> follower_goal_pos;
 
+void fol_goal_posfn(int x,int y)
+{
+switch (fid_id)
+{
+case 0:
+  follower_goal_pos[0][0] = x;
+  follower_goal_pos[0][1] = y;
+  break;
+case 1:
+  follower_goal_pos[1][0] = x;
+  follower_goal_pos[1][1] = y;
+  break;
+case 2:
+  follower_goal_pos[2][0] = x;
+  follower_goal_pos[2][1] = y;
+  break;
+case 3:
+  follower_goal_pos[3][0] = x;
+  follower_goal_pos[3][1] = y;
+  break;
+default:
+  break;
+}
+}
 void listen(tf2_ros::Buffer& tfBuffer) {
   //for listener
 
@@ -50,6 +78,7 @@ void listen(tf2_ros::Buffer& tfBuffer) {
       << trans_y << ","
       << trans_z << "]"
     );
+    fol_goal_posfn(trans_x,trans_y);
   }
   catch (tf2::TransformException& ex) {
     ROS_WARN("%s", ex.what());
@@ -57,22 +86,15 @@ void listen(tf2_ros::Buffer& tfBuffer) {
   }
 }
 
-bool detect()
-{
-if(fid_id!= 10)
-{
-  return true;
-}
-else
-{
-  return false;
-}
-}
+bool detect;
+int goal_reach = 0;
+
+
+
 
 void arsubcallback(const fiducial_msgs::FiducialTransformArray::ConstPtr &msgs){
-ROS_INFO("Callback call");
 if (!msgs->transforms.empty()) {//check marker is detected
-//broadcaster object
+// broadcaster object
 static tf2_ros::TransformBroadcaster br;
 geometry_msgs::TransformStamped transformStamped;
 //broadcast the new frame to /tf Topic
@@ -82,8 +104,14 @@ transformStamped.child_frame_id = "marker_frame";
 transformStamped.transform.translation.x = msgs->transforms[0].transform.translation.x;
 /*write the remaining code here*/
 fid_id = msgs->transforms[0].fiducial_id;
-
-br.sendTransform(transformStamped);
+detect = true;
+ROS_INFO("Detect true");
+// br.sendTransform(transformStamped);
+}
+else
+{
+  detect = false;
+  ROS_INFO("Detect false");
 }
 ROS_INFO("Callback call exit");
 }
@@ -96,7 +124,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "simple_navigation_goals");
   ros::NodeHandle nh;
 
-  ros::Subscriber arsub = nh.subscribe("fiducial_transforms",1000, arsubcallback);;
+  ros::Subscriber arsub = nh.subscribe("fiducial_transforms",100, arsubcallback);
   ros::Publisher pubex=nh.advertise<geometry_msgs::Twist>("explorer/cmd_vel", 100);
   ros::Publisher pubfl=nh.advertise<geometry_msgs::Twist>("follower/cmd_vel", 100);
   // tell the action client that we want to spin a thread by default
@@ -123,29 +151,45 @@ int main(int argc, char** argv)
   nh.getParam("simple_navigation_goals/aruco_lookup_locations/target_3", my_goal3);
   nh.getParam("simple_navigation_goals/aruco_lookup_locations/target_4", my_goal4);
   
-  move_base_msgs::MoveBaseGoal explorer_goal;
-  move_base_msgs::MoveBaseGoal follower_goal;
+  // std::array<std::array<int,2>,5> ex_goal_pos = {my_goal1,my_goal2,my_goal3,my_goal4};
+
+  move_base_msgs::MoveBaseGoal explorer_goal[4];
+  move_base_msgs::MoveBaseGoal follower_goal[4];
   
 
+  
+    explorer_goal[0].target_pose.pose.position.x = my_goal1[0];
+    explorer_goal[0].target_pose.pose.position.y = my_goal1[1];
 
-  //Build goal for explorer
-  explorer_goal.target_pose.header.frame_id = "map";
-  explorer_goal.target_pose.header.stamp = ros::Time::now();
-  // explorer_goal.target_pose.pose.position.x = 7.710214;//
-  // explorer_goal.target_pose.pose.position.y = -1.716889;//
-  explorer_goal.target_pose.pose.position.x = my_goal1[0];
-  explorer_goal.target_pose.pose.position.y = my_goal1[1];
-  explorer_goal.target_pose.pose.orientation.w = 1.0;
+    explorer_goal[1].target_pose.pose.position.x = my_goal2[0];
+    explorer_goal[1].target_pose.pose.position.y = my_goal2[1];
 
-  //Build goal for follower
-  follower_goal.target_pose.header.frame_id = "map";
-  follower_goal.target_pose.header.stamp = ros::Time::now();
-  follower_goal.target_pose.pose.position.x = -0.289296;//
-  follower_goal.target_pose.pose.position.y = -1.282680;//
-  follower_goal.target_pose.pose.orientation.w = 1.0;
+    explorer_goal[2].target_pose.pose.position.x = my_goal3[0];
+    explorer_goal[2].target_pose.pose.position.y = my_goal3[1];
+    
+    explorer_goal[3].target_pose.pose.position.x = my_goal4[0];
+    explorer_goal[3].target_pose.pose.position.y = my_goal4[1];
 
+    explorer_goal[4].target_pose.pose.position.x = -4;
+    explorer_goal[4].target_pose.pose.position.y = 2.5;
 
+  for(int i=0; i<4;i++)
+  {
+    explorer_goal[i].target_pose.header.frame_id = "map";
+    explorer_goal[i].target_pose.header.stamp = ros::Time::now();
+    explorer_goal[i].target_pose.pose.orientation.w = 1.0;
+  }
 
+  for(int i = 0; i<4;i++)
+  {
+    follower_goal[i].target_pose.header.frame_id = "map";
+    follower_goal[i].target_pose.header.stamp = ros::Time::now();
+    follower_goal[i].target_pose.pose.position.x = follower_goal_pos[i][0];//
+    follower_goal[i].target_pose.pose.position.y = follower_goal_pos[i][1];//
+    follower_goal[i].target_pose.pose.orientation.w = 1.0;
+  }
+    follower_goal[4].target_pose.pose.position.x = -4;//
+    follower_goal[4].target_pose.pose.position.y = 3.5;//
   // explorer_client.waitForResult();
 
   // ROS_INFO("Sending goal");
@@ -156,36 +200,53 @@ int main(int argc, char** argv)
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
   ros::Rate loop_rate(10);
-
+  int cnt_ex = 0;
+  int cnt_fl = 0;
   while (ros::ok()) {
-    int goal_reach = 0;
-    if (!explorer_goal_sent)     {
+    
+    if (!explorer_goal_sent && cnt_ex<5)
+    {
+      std::cout<<"Sending goal";
       ROS_INFO("Sending goal for explorer");
-      // ROS_INFO("%f\n",my_goal[0]);
-      // ROS_INFO("%f\n",my_goal[1]);
-      explorer_client.sendGoal(explorer_goal);//this should be sent only once
+      explorer_client.sendGoal(explorer_goal[cnt_ex]);//this should be sent only once
       explorer_goal_sent = true;
     }
-    if (explorer_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-        while(!detect())
+    if (explorer_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED && cnt_ex<5) {
+      cnt_ex++;
+      if(cnt_ex<4)
+      {
+        explorer_goal_sent = false;
+        while(!detect)
         {
           geometry_msgs::Twist msg;
-          msg.linear.x = 0;
-          msg.linear.y = 0;
           msg.angular.z = 0.1;
           pubex.publish(msg);
+          ros::spinOnce();
+          loop_rate.sleep();
         }
-      goal_reach ++;
+      }
       ROS_INFO("Hooray, robot reached goal");
     }
-    // if (!follower_goal_sent) {
-    //   ROS_INFO("Sending goal for follower");
-    //   follower_client.sendGoal(follower_goal);//this should be sent only once
-    //   follower_goal_sent = true;
-    // }
-    // if (follower_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    //   ROS_INFO("Hooray, robot reached goal");
-    // }
+    if(cnt_ex == 5)
+    {
+          geometry_msgs::Twist msg;
+          msg.angular.z = 0;
+          pubex.publish(msg);
+    }
+    if (!follower_goal_sent && cnt_ex==5 && cnt_fl<5) {
+      ROS_INFO("Sending goal for follower");
+      follower_client.sendGoal(follower_goal[cnt_fl]);//this should be sent only once
+      follower_goal_sent = true;
+    }
+    if (follower_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED && cnt_fl!=5) {
+      follower_goal_sent = false;
+      cnt_fl++;
+      ROS_INFO("Hooray, robot reached goal");
+    }
+    if(cnt_fl == 5)
+    {
+      ros::shutdown();
+    }
     broadcast();
     listen(tfBuffer);
     ros::spinOnce(); //uncomment this if you have subscribers in your code
